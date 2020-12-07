555e302872798398fdbccedc101ca9d35ccc8a64
Otto Porkka <otto.porkka@iki.fi>
2020-12-07 22:13:22 +0200

    Lamport works if all nodes keep online

--- client/src/lamportClock.js
+++ client/src/lamportClock.js
@@ -2,17 +2,9 @@ const axios = require('axios')
 
 let lamportClock = 1
 
-exports.onReceive = async (ctx, next) => {
-    const body = ctx.request.body
-    console.log(`${(ctx.request.method)} "${ctx.request.url}" FROM ${ctx.request.ip}: ${JSON.stringify(body)}`)
-    if (!body) await next()
-    if (body.lamportClock) {
-
-    }
-    await next()
-}
-
-exports.multicast = async (data) => {
+const multicast = async (data) => {
+    // Set own clock when sending
+    lamportClock++
     for (let client of global.clientList) {
         try {
             // Getting initial state doesn't need lamport clock
@@ -40,6 +32,67 @@ exports.multicast = async (data) => {
             }
         }
     }
+}
+
+const checkAcks = () => {
+    const first = global.eventQueue[0]
+    if (first.ackList.every((client) => (client.ack === true))) {
+        console.log('All ACKs received for the first entry in the queue!')
+        global.playlist.push(first.song)
+        global.eventQueue.shift()
+    }
+}
+
+exports.onReceive = async (ctx, next) => {
+    const body = ctx.request.body
+    const senderIp = ctx.request.ip.split(':')[3]
+    console.log(`${(ctx.request.method)} "${ctx.request.url}" FROM ${senderIp}: ${JSON.stringify(body)}`)
+    // Skip to next middleware if no body
+    if (!body) await next()
+
+    if (body.lamportClock) {
+        // Adjust own clock on receive
+        lamportClock = (body.lamportClock > lamportClock) ? body.lamportClock +1 : lamportClock +1
+        switch (body.type) {
+            case "ADD_SONG":
+                global.eventQueue.push({
+                    clock: lamportClock,
+                    song: body.message,
+                    by: senderIp,
+                    ackList: global.clientList.map((client) => ({client:client, ack: false}))
+                })
+                global.eventQueue.sort((a, b) => a.clock.localeCompare(b.clock))
+                multicast({type: "ACK_SONG", song: body.message, by: senderIp})
+                ctx.body = { message: `${body.message} added to eventQueue.` }
+                ctx.status = 200
+                return
+            case "ACK_SONG":
+                const entry = global.eventQueue.find((e) => (e.song === body.song && e.by === body.by))
+                if (entry) {
+                    const clientAck = entry.ackList.find((e) => (e.client === senderIp))
+                    clientAck.ack = true
+                    checkAcks()
+                    ctx.body = { message: "OK!" }
+                    ctx.status = 200
+                    return
+                } else {
+                    // Not yet received initial request, storing ack anyway
+                    // TODO
+                    global.eventQueue.push({
+                        clock: undefined,
+                        song: body.song,
+                        by: body.by,
+                        ackList: global.clientList.map((client) => ({client:client, ack: false}))
+                    })
+                }
+                break
+        }
+    }
+    await next()
+}
+
+exports.multicast = async (data) => {
+    return multicast(data)
 }
 
 exports.getTimeStamp = () => {
